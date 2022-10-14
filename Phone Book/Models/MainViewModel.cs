b3757d7e37e11faf997bc099aeceb8b87122064e
fgvngfdf <fgvngfdf@gmail.com>
2022-10-15 02:28:25 +0300

    Доработка логики работы

--- Phone Book/Models/MainViewModel.cs
+++ Phone Book/Models/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Windows.Input;
 using Phone_Book.Infrastructure.WindowClose;
 using System;
+using System.Linq;
 
 namespace Phone_Book.Models
 {
@@ -22,15 +23,15 @@ namespace Phone_Book.Models
 				switch (result)
 				{
 					case 1:
-						if (string.IsNullOrEmpty(fileWay))
+						if (string.IsNullOrEmpty(FileWay))
 						{
-							fileWay = dialogService.SaveJsonFile();
-							if (string.IsNullOrEmpty(fileWay)) return false;
+							FileWay = dialogService.SaveJsonFile();
+							if (string.IsNullOrEmpty(FileWay)) return false;
 							else
 							{
 								try
 								{
-									phoneBook.Save(fileWay);
+									phoneBook.Save(FileWay);
 								}
 								catch (Exception ex)
 								{
@@ -44,7 +45,7 @@ namespace Phone_Book.Models
 						{
 							try
 							{
-								phoneBook.Save(fileWay);
+								phoneBook.Save(FileWay);
 							}
 							catch (Exception ex)
 							{
@@ -67,7 +68,10 @@ namespace Phone_Book.Models
 		public ICommand OpenAbonentInfoDialog { get; private set; }
 		public ICommand OpenAddGroupDialog { get; private set; }
 		public ICommand OpenEditGroupDialog { get; private set; }
+		public ICommand DeleteGroupCommand { get; private set; }
 		public ICommand LoadFileCommand { get; private set; }
+		public ICommand SaveFileCommand { get; private set; }
+		public ICommand SaveFileAsCommand { get; private set; }
 
 		private IDialogService dialogService;
 		private IPhoneBook phoneBook;
@@ -76,7 +80,7 @@ namespace Phone_Book.Models
 		private AbonentModel selectAbonent;
 		private AbonentModel selectAbonentInGroup;
 		private string selectGroup;
-		private string fileWay;
+		public string fileWay;
 
 		public IReadOnlyCollection<AbonentModel> Abonents
 		{
@@ -115,7 +119,15 @@ namespace Phone_Book.Models
 		public string SelectGroup
 		{
 			get => selectGroup;
-			set => Set(ref selectGroup, value);
+			set
+			{
+				if (Set(ref selectGroup, value)) GlobalSelectGroup = value;
+			}
+		}
+		public string FileWay
+		{
+			get => fileWay;
+			set => Set(ref fileWay, value);
 		}
 
 		public MainViewModel(IPhoneBook book, IDialogService dialogService)
@@ -133,7 +145,91 @@ namespace Phone_Book.Models
 			OpenAddGroupDialog = new CommandBase(OnOpenAddGroupDialog, (obj) => true);
 			OpenEditAbonentDialog = new CommandBase(OnOpenEditAbonentDialog, (obj) => SelectAbonent != null);
 			OpenAbonentInfoDialog = new CommandBase(OnOpenAbonentInfoDialog, (obj) => GlobalSelectAbonent != null);
+			DeleteGroupCommand = new CommandBase(OnDeleteGroup, (obj) => !string.IsNullOrEmpty(SelectGroup));
+			OpenEditGroupDialog = new CommandBase(OnEditGroup, (obj) => !string.IsNullOrEmpty(GlobalSelectGroup) && Abonents.Where(i => i.Groups.Contains(GlobalSelectGroup)).Count() > 0);
 			LoadFileCommand = new CommandBase(OnLoadFile, (obj) => true);
+			SaveFileCommand = new CommandBase(OnSaveFile, (obj) => true);
+			SaveFileAsCommand = new CommandBase(OnSaveAsCommand, (obj) => true);
+		}
+
+		private void OnEditGroup(object obj)
+		{
+			bool result = false;
+			dialogService.ShowDialog(EnumDialogsWindowsFromGroup.EditGroups, (value) => result = value);
+			Abonents = phoneBook.GetAbonents();
+			Groups = phoneBook.GetGroupsName();
+		}
+
+		private void OnDeleteGroup(object obj)
+		{
+			try
+			{
+				phoneBook.RemoveGroup(SelectGroup);
+				SelectGroup = string.Empty;
+			}
+			catch (Exception ex)
+			{
+				dialogService.ShowMessageException(ex.InnerException is null ? ex.Message : ex.InnerException.Message, "Ошибка");
+			}
+		}
+
+		private void OnSaveAsCommand(object obj)
+		{
+			if (Abonents.Count == 0)
+			{
+				dialogService.ShowMessageInfo("Нет абонентов для сохранения", "Сохранение");
+			}
+			else
+			{
+				string newfilePath = dialogService.SaveJsonFile();
+				if (!string.IsNullOrEmpty(newfilePath))
+				{
+					try
+					{
+						phoneBook.Save(newfilePath);
+						FileWay = newfilePath;
+					}
+					catch (Exception ex)
+					{
+						dialogService.ShowMessageException(ex.InnerException is null ? ex.Message : ex.InnerException.Message, "Ошибка");
+					}
+				}
+			}
+		}
+
+		private void OnSaveFile(object obj)
+		{
+			if (Abonents.Count == 0)
+			{
+				dialogService.ShowMessageInfo("Нет абонентов для сохранения", "Сохранение");
+			}
+			else if (string.IsNullOrEmpty(FileWay))
+			{
+				string fileWay = dialogService.SaveJsonFile();
+				if (string.IsNullOrEmpty(fileWay)) return;
+				else
+				{
+					try
+					{
+						phoneBook.Save(fileWay);
+					}
+					catch (Exception ex)
+					{
+						dialogService.ShowMessageException(ex.InnerException is null ? ex.Message : ex.InnerException.Message, "Ошибка");
+					}
+				}
+			}
+			else
+			{
+				try
+				{
+					phoneBook.Save(FileWay);
+				}
+				catch (Exception ex)
+				{
+					dialogService.ShowMessageException(ex.InnerException is null ? ex.Message : ex.InnerException.Message, "Ошибка");
+				}
+			}
 		}
 
 		private void OnOpenAddAbonentDialog(object obj)
@@ -146,9 +242,13 @@ namespace Phone_Book.Models
 
 		private void OnOpenAddGroupDialog(object obj)
 		{
-			bool result = false;
-			dialogService.ShowDialog(EnumDialogsWindowsFromGroup.AddGroup, (value) => result = value);
-			Abonents = phoneBook.GetAbonents();
+			if (Abonents.Count != 0)
+			{
+				bool result = false;
+				dialogService.ShowDialog(EnumDialogsWindowsFromGroup.AddGroup, (value) => result = value);
+				Abonents = phoneBook.GetAbonents();
+				Groups = phoneBook.GetGroupsName();
+			}
 		}
 
 		private void OnOpenEditAbonentDialog(object obj)
@@ -174,14 +274,14 @@ namespace Phone_Book.Models
 				switch (result)
 				{
 					case 1:
-						if (string.IsNullOrEmpty(fileWay))
+						if (string.IsNullOrEmpty(FileWay))
 						{
-							fileWay = dialogService.SaveJsonFile();
-							if (string.IsNullOrEmpty(fileWay)) return;
+							FileWay = dialogService.SaveJsonFile();
+							if (string.IsNullOrEmpty(FileWay)) return;
 
 							try
 							{
-								phoneBook.Save(fileWay);
+								phoneBook.Save(FileWay);
 							}
 							catch (Exception ex)
 							{
@@ -199,12 +299,12 @@ namespace Phone_Book.Models
 				}
 			}
 
-			fileWay = dialogService.OpenJsonFile();
-			if (!string.IsNullOrEmpty(fileWay))
+			FileWay = dialogService.OpenJsonFile();
+			if (!string.IsNullOrEmpty(FileWay))
 			{
 				try
 				{
-					phoneBook.Load(fileWay);
+					phoneBook.Load(FileWay);
 				}
 				catch (Exception ex)
 				{
