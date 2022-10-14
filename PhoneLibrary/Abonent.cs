ef333347f88df77f574d57cf40b0869a85ff8b7d
fgvngfdf <fgvngfdf@gmail.com>
2022-10-14 23:11:36 +0300

    Удалены неиспользуемые методы

--- PhoneLibrary/Abonent.cs
+++ PhoneLibrary/Abonent.cs
@@ -1,6 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-#pragma warning disable CS8618, CS8765
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhoneLibrary
 {
@@ -9,29 +8,31 @@ namespace PhoneLibrary
 		private List<string> _groups { get; set; }
 		private List<PhoneNumber> _phoneNumbers { get; set; }
 
-		public string Name { get; private set; }
-		public string Surname { get; private set; }
-		public DateTime? DateOfBirth { get; private set; }
-		public string Residence { get; private set; }
-		public int Id { get; private set; }
-		public string ImageBase64 { get; private set; }
+		internal string Name { get; private set; }
+		internal string Surname { get; private set; }
+		internal DateTime? DateOfBirth { get; private set; }
+		internal string Residence { get; private set; }
+		internal int Id { get; private set; }
+		internal string ImageBase64 { get; private set; }
+		internal IReadOnlyCollection<string> Groups { get; }
+		internal IReadOnlyCollection<PhoneNumber> PhoneNumbers { get; }
 
 		public Abonent(AbonentModel model)
 		{
 			ValidateAbonentModel(model);
-			SetPropertyByModel(model);
-		}
 
-		public void AddPhones(PhoneNumberModel phoneModel)
-		{
-			var phone = new PhoneNumber(phoneModel);
-			if (!_phoneNumbers.Contains(phone)) _phoneNumbers.Add(phone);
-		}
+			Name = model.Name;
+			Surname = model.Surname;
+			DateOfBirth = model.DateOfBirth;
+			Residence = model.Residence;
+			ImageBase64 = model.ImageBase64;
+			Id = (int)model.Id;
 
-		public void RemovePhones(PhoneNumberModel phoneModel)
-		{
-			var phone = new PhoneNumber(phoneModel);
-			_phoneNumbers.Remove(phone);
+			_groups = model.Groups == null ? new List<string>() : ((string[])model.Groups.Clone()).ToList();
+			_phoneNumbers = model.Phones.Select(item => new PhoneNumber(item)).ToList();
+			
+			PhoneNumbers = new ReadOnlyCollection<PhoneNumber>(_phoneNumbers);
+			Groups = new ReadOnlyCollection<string>(_groups);
 		}
 
 		public void AddGroup(string group)
@@ -44,15 +45,6 @@ namespace PhoneLibrary
 			_groups.Remove(group);
 		}
 
-		public void RenameAnonent(AbonentModel model)
-		{
-			ValidateAbonentModel(model);
-			if (model.Id == null) throw new ArgumentNullException($"Отсутствует индентификатор");
-			if (model.Id != Id) throw new InvalidOperationException("Идентификатор модели не соответствует идентификатору объекта");
-
-			SetPropertyByModel(model);
-		}
-
 		public AbonentModel GetModel()
 		{
 			return new AbonentModel()
@@ -62,7 +54,7 @@ namespace PhoneLibrary
 				DateOfBirth = DateOfBirth,
 				Residence = Residence,
 				Groups = (string[])_groups.ToArray().Clone(),
-				ImageBase64 = this.ImageBase64,
+				ImageBase64 = ImageBase64,
 				Phones = _phoneNumbers.Select(item => item.GetModel()).ToArray(),
 				Id = Id
 			};
@@ -77,34 +69,22 @@ namespace PhoneLibrary
 			if (!Validator.TryValidateObject(model, context, validationResults, true)) throw new ValidationException(validationResults[0].ErrorMessage);
 		}
 
-		private void SetPropertyByModel(AbonentModel model)
+		public override bool Equals(object? obj)
 		{
-			Name = model.Name;
-			Surname = model.Surname;
-			DateOfBirth = model.DateOfBirth;
-			Residence = model.Residence;
-			ImageBase64 = model.ImageBase64;
-			_groups = model.Groups == null ? new List<string>() : ((string[])model.Groups.Clone()).ToList();
-			_phoneNumbers = model.Phones.Select(item => new PhoneNumber(item)).ToList();
-		}
-
-		public override bool Equals(object obj)
-		{
-			if (obj is Abonent abonent)
+			if (obj is not null)
 			{
-				return string.Equals(Name, abonent.Name) && string.Equals(Surname, abonent.Surname)
-														 && string.Equals(Residence, abonent.Residence)
-														 && Equals(DateOfBirth, abonent.DateOfBirth)
-														 && Id == abonent.Id;
-			}
-			else if (obj is AbonentModel abonentModel)
-			{
-				bool equalsResult = string.Equals(Name, abonentModel.Name) && string.Equals(Surname, abonentModel.Surname)
-														 && string.Equals(Residence, abonentModel.Residence)
-														 && Equals(DateOfBirth, abonentModel.DateOfBirth);
-
-				if (abonentModel.Id is null) return equalsResult;
-				return equalsResult && Id == abonentModel.Id;
+				if (obj is Abonent abonent)
+				{
+					return string.Equals(Name, abonent.Name) && string.Equals(Surname, abonent.Surname)
+															 && string.Equals(Residence, abonent.Residence)
+															 && Equals(DateOfBirth, abonent.DateOfBirth);
+				}
+				else if (obj is AbonentModel abonentModel)
+				{
+					return string.Equals(Name, abonentModel.Name) && string.Equals(Surname, abonentModel.Surname)
+															 && string.Equals(Residence, abonentModel.Residence)
+															 && Equals(DateOfBirth, abonentModel.DateOfBirth);
+				}
 			}
 			return false;
 		}
