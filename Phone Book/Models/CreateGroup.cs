5a439cc2859308997d815e454e4de38192a6e417
fgvngfdf <fgvngfdf@gmail.com>
2022-10-14 23:16:39 +0300

    Добавлена логика работы модели представления

--- Phone Book/Models/CreateGroup.cs
+++ Phone Book/Models/CreateGroup.cs
@@ -1,36 +1,161 @@
-﻿using Phone_Book.Models.Base;
+﻿using Phone_Book.Infrastructure.Command;
+using Phone_Book.Infrastructure.DialogService;
+using Phone_Book.Infrastructure.DialogService.Abonents;
+using Phone_Book.Infrastructure.WindowClose;
+using Phone_Book.Models.Base;
 using PhoneLibrary;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 
 namespace Phone_Book.Models
 {
-	internal class CreateGroup : BaseViewModel
+	internal class CreateGroup : BaseViewModel, IWindowClose
 	{
-		private IPhoneBook _phoneBook;
+		IDialogAbonentService dialogService;
+		private IPhoneBook phoneBook;
 		private string _groupName;
-		private IEnumerable<AbonentModel> abonents;
+		private IReadOnlyCollection<AbonentModel> phoneBookAbonents;
+		private AbonentModel selectAbonentInPhoneBook;
+		private AbonentModel selectAbonentInSelectedAbonents;
+		private bool isAddGroup;
 
 		public string GroupName
 		{
 			get => _groupName;
 			set => Set(ref _groupName, value);
 		}
-		public IEnumerable<AbonentModel> Abonents
+		public IReadOnlyCollection<AbonentModel> PhoneBookAbonents
 		{
-			get => abonents;
-			set
-			{
-				if (abonents == value) OnPropertyChanged(nameof(Abonents));
-				else Set(ref abonents, value);
-			}
+			get => phoneBookAbonents;
+			set => Set(ref phoneBookAbonents, value);
 		}
 		public ObservableCollection<AbonentModel> SelectAbonents { get; set; }
-
-		public CreateGroup(IPhoneBook book)
+		public AbonentModel SelectAbonentInPhoneBook
 		{
-			_phoneBook = book;
+			get => selectAbonentInPhoneBook;
+			set
+			{
+				if (Set(ref selectAbonentInPhoneBook, value)) GlobalSelectAbonent = value;
+			}
+		}
+		public AbonentModel SelectAbonentInSelectedAbonents
+		{
+			get => selectAbonentInSelectedAbonents;
+			set
+			{
+				if (Set(ref selectAbonentInSelectedAbonents, value)) GlobalSelectAbonent = value;
+			}
+		}
+
+		public ICommand AddAbonentInGroup { get; private set; }
+		public ICommand DeleteAbonentInGroup { get; private set; }
+		public ICommand EditAbonentCommand { get; private set; }
+		public ICommand ViewAbonentCommand { get; private set; }
+		public ICommand AddGroupInPhoneBookCommand { get; private set; }
+		public ICommand CloseCommand { get; private set; }
+		public Action Close { get; set; }
+
+		public CreateGroup(IPhoneBook book, IDialogAbonentService dialog)
+		{
+			phoneBook = book;
+			dialogService = dialog;
+
+			PhoneBookAbonents = book.GetAbonents();
 			SelectAbonents = new ObservableCollection<AbonentModel>();
+			CommandInitialize();
+		}
+
+		private void CommandInitialize()
+		{
+			AddAbonentInGroup = new CommandBase(OnAddAbonentInGroup, (obj) => SelectAbonentInPhoneBook is not null && !SelectAbonents.Contains(SelectAbonentInPhoneBook));
+			DeleteAbonentInGroup = new CommandBase(OnDeleteInGroup, (obj) => SelectAbonentInSelectedAbonents is not null && SelectAbonents.Contains(SelectAbonentInSelectedAbonents));
+			EditAbonentCommand = new CommandBase(OnEditAbonent, (obj) => SelectAbonentInSelectedAbonents is not null || SelectAbonentInPhoneBook is not null);
+			ViewAbonentCommand = new CommandBase(OnViewAbonent, (obj) => SelectAbonentInSelectedAbonents is not null || SelectAbonentInPhoneBook is not null);
+			AddGroupInPhoneBookCommand = new CommandBase(OnAddGroupInPhone, (obj) => !string.IsNullOrEmpty(GroupName) && SelectAbonents.Count > 0);
+			CloseCommand = new CommandBase(OnClose, (obj) => true);
+		}
+
+		private void OnAddAbonentInGroup(object obj)
+		{
+			SelectAbonents.Add(SelectAbonentInPhoneBook);
+			SelectAbonentInPhoneBook = null;
+			OnPropertyChanged(nameof(PhoneBookAbonents));
+		}
+
+		private void OnDeleteInGroup(object obj)
+		{
+			SelectAbonents.Remove(SelectAbonentInSelectedAbonents);
+			SelectAbonentInSelectedAbonents = null;
+			OnPropertyChanged(nameof(PhoneBookAbonents));
+		}
+
+		private void OnEditAbonent(object obj)
+		{
+			bool result = false;
+			dialogService.ShowDialog(EnumDialogsWindowsFromAbonent.EditAbonent, (value) => result = value);
+			PhoneBookAbonents = phoneBook.GetAbonents();
+			OnPropertyChanged(nameof(SelectAbonents));
+		}
+
+		private void OnViewAbonent(object obj)
+		{
+			bool result = false;
+			dialogService.ShowDialog(EnumDialogsWindowsFromAbonent.AbonentInfo, (value) => result = value);
+		}
+
+		private void OnAddGroupInPhone(object obj)
+		{
+			isAddGroup = true;
+			Close?.Invoke();
+			isAddGroup = false;
+		}
+
+		private void OnClose(object obj)
+		{
+			isAddGroup = false;
+			Close?.Invoke();
+		}
+
+		public bool CanClose()
+		{
+			if (isAddGroup)
+			{
+				if (string.IsNullOrEmpty(GroupName) || SelectAbonents.Count == 0) return true;
+				else
+				{
+					if (phoneBook.GetGroupsName().Contains(GroupName))
+					{
+						dialogService.ShowMessageInfo($"Группа с названием '{GroupName}' уже существует", "Внимание");
+						return false;
+					}
+					try
+					{
+						phoneBook.AddGroup(GroupName, SelectAbonents.ToArray());
+					}
+					catch (Exception ex)
+					{
+						dialogService.ShowMessageException(ex.InnerException is null ? ex.Message : ex.InnerException.Message, "Ошибка");
+						return false;
+					}
+					return true;
+				}
+			}
+			else
+			{
+				if (string.IsNullOrEmpty(GroupName) && SelectAbonents.Count == 0) return true;
+				int result = dialogService.ShowMessageQuestion("Закрыть окно создания группы?", "Закрыть окно");
+
+				switch (result)
+				{
+					case 1:
+						return true;
+					default:
+						return false;
+				}
+			}
 		}
 	}
 }
--- Phone Book/Models/EditAbonent.cs
+++ Phone Book/Models/EditAbonent.cs
@@ -1,8 +1,297 @@
-﻿using Phone_Book.Models.Base;
+﻿using Phone_Book.Infrastructure.Command;
+using Phone_Book.Infrastructure.DialogService;
+using Phone_Book.Models.Base;
+using PhoneLibrary;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System;
+using Phone_Book.Infrastructure.WindowClose;
+using System.Windows.Controls;
+using System.Linq;
 
 namespace Phone_Book.Models
 {
-	internal class EditAbonent : BaseViewModel
+	internal class EditAbonent : BaseViewModel, IWindowClose
 	{
+		public bool CanClose()
+		{
+			if (model is not null)
+			{
+				return TryUpdateUser(model);
+			}
+			else
+			{
+				int result = dialogs.ShowMessageQuestion("Закрыть окно редактирования пользователя?", "Выход");
+
+				switch (result)
+				{
+					case 1:
+						return true;
+					default:
+						return false;
+				}
+			}
+		}
+		public Action Close { get; set; }
+
+		private AbonentModel? model;
+		private IDialog dialogs;
+		private IPhoneBook phoneBook;
+		private IReadOnlyCollection<string> phoneBookGroups;
+		private IReadOnlyCollection<string> phoneTypeCollection;
+		private PhoneNumberModel selectPhone;
+		private string surname;
+		private string name;
+		private string residence;
+		private DateTime? dateOfBirth;
+		private string selectPhoneBookGroup;
+		private string selectAbonentGroup;
+		private string inputNewPhone;
+		private string inputNewType;
+		private string selectType;
+		private string abonentImageBase64;
+		private Image abonentImage;
+
+		public string Name
+		{
+			get => name;
+			set
+			{
+				if (!string.Equals(name, value, StringComparison.OrdinalIgnoreCase)) name = value;
+			}
+		}
+		public string Surname
+		{
+			get => surname;
+			set
+			{
+				if (!string.Equals(surname, value, StringComparison.OrdinalIgnoreCase)) surname = value;
+			}
+		}
+		public string Residence
+		{
+			get => residence;
+			set
+			{
+				if (!string.Equals(residence, value)) residence = value;
+			}
+		}
+		public string SelectPhoneBookGroup
+		{
+			get => selectPhoneBookGroup;
+			set => Set(ref selectPhoneBookGroup, value);
+		}
+		public string SelectAbonentGroup
+		{
+			get => selectAbonentGroup;
+			set => Set(ref selectAbonentGroup, value);
+		}
+		public string InputNewPhone
+		{
+			get => inputNewPhone;
+			set => Set(ref inputNewPhone, value);
+		}
+		public string InputNewType
+		{
+			get => inputNewType;
+			set => Set(ref inputNewType, value);
+		}
+		public string SelectType
+		{
+			get => selectType;
+			set => Set(ref selectType, value);
+		}
+		public DateTime? DateOfBirth
+		{
+			get => dateOfBirth;
+			set
+			{
+				if (!Equals(dateOfBirth, value)) dateOfBirth = value;
+			}
+		}
+		public ObservableCollection<PhoneNumberModel> Phones { get; }
+		public ObservableCollection<string> SelectedGroups { get; }
+		public IReadOnlyCollection<string> PhoneBookGroups
+		{
+			get => phoneBookGroups;
+			set
+			{
+				if (phoneBookGroups != value) Set(ref phoneBookGroups, value);
+				else OnPropertyChanged(nameof(PhoneBookGroups));
+			}
+		}
+		public IReadOnlyCollection<string> PhoneTypeCollection
+		{
+			get => phoneTypeCollection;
+			set
+			{
+				if (phoneTypeCollection != value) Set(ref phoneTypeCollection, value);
+				else OnPropertyChanged(nameof(PhoneTypeCollection));
+			}
+		}
+		private ObservableCollection<string> Groups { get; }
+		public PhoneNumberModel SelectPhone
+		{
+			get => selectPhone;
+			set => Set(ref selectPhone, value);
+		}
+		public Image AbonentImage
+		{
+			get => abonentImage;
+			set => Set(ref abonentImage, value);
+		}
+
+
+		public ICommand AddAbonentInPhoneBook { get; private set; }
+		public ICommand CloseWindow { get; private set; }
+		public ICommand AddPhoneCommand { get; private set; }
+		public ICommand DeletePhoneCommand { get; private set; }
+		public ICommand AddGroupCommand { get; private set; }
+		public ICommand DeleteGroupCommand { get; private set; }
+		public ICommand AddImageCommand { get; private set; }
+		public ICommand DeleteImageCommand { get; private set; }
+
+		public EditAbonent(IPhoneBook phoneBook, IDialog dialogWindows)
+		{
+			dialogs = dialogWindows;
+			this.phoneBook = phoneBook;
+			SelectedGroups = new();
+			PhoneBookGroups = this.phoneBook.GetGroupsName();
+			PhoneTypeCollection = this.phoneBook.GetPhoneTypes();
+
+			Name = GlobalSelectAbonent.Name;
+			Surname = GlobalSelectAbonent.Surname;
+			Residence = GlobalSelectAbonent.Residence;
+			DateOfBirth = GlobalSelectAbonent.DateOfBirth;
+			Phones = new ObservableCollection<PhoneNumberModel>(GlobalSelectAbonent.Phones);
+			Groups = new ObservableCollection<string>(GlobalSelectAbonent.Groups);
+			if (GlobalSelectAbonent.ImageBase64 is not null)
+			{
+				TryAddImage(Convert.FromBase64String(GlobalSelectAbonent.ImageBase64));
+			}
+
+			CommandInitialize();
+		}
+
+		private void CommandInitialize()
+		{
+			DeleteGroupCommand = new CommandBase(OnDeleteGroup, (obj) => !string.IsNullOrEmpty(SelectAbonentGroup) && SelectedGroups.Contains(SelectAbonentGroup));
+			DeletePhoneCommand = new CommandBase(OnDeletePhone, (obj) => Phones.Contains(SelectPhone));
+			AddGroupCommand = new CommandBase(OnAddGroup, (obj) => !string.IsNullOrEmpty(SelectPhoneBookGroup) && !SelectedGroups.Contains(SelectPhoneBookGroup));
+			AddPhoneCommand = new CommandBase(OnAddPhone, (obj) => (!string.IsNullOrEmpty(SelectType) || !string.IsNullOrEmpty(InputNewType)) && !string.IsNullOrEmpty(InputNewPhone));
+			AddAbonentInPhoneBook = new CommandBase(OnAddAbonentInPhoneBook, (obj) => !string.IsNullOrEmpty(Name) && Phones.Count != 0);
+			CloseWindow = new CommandBase(OnCloseWindow, (obj) => true);
+			AddImageCommand = new CommandBase(OnAddImage, (obj) => true);
+			DeleteImageCommand = new CommandBase(OnDeleteImage, (obj) => !string.IsNullOrEmpty(abonentImageBase64));
+		}
+
+		private void OnDeleteGroup(object obj)
+		{
+			SelectedGroups.Remove(SelectAbonentGroup);
+			SelectAbonentGroup = string.Empty;
+			OnPropertyChanged(SelectAbonentGroup);
+		}
+
+		private void OnDeletePhone(object obj)
+		{
+			Phones.Remove(SelectPhone);
+			SelectPhone = null;
+		}
+
+		private void OnAddGroup(object obj)
+		{
+			SelectedGroups.Add(SelectPhoneBookGroup);
+			OnPropertyChanged(nameof(PhoneBookGroups));
+			SelectPhoneBookGroup = string.Empty;
+		}
+
+		private void OnAddPhone(object obj)
+		{
+			var newPhone = new PhoneNumberModel() { Type = string.IsNullOrEmpty(InputNewType) ? SelectType : InputNewType, Number = InputNewPhone };
+			try
+			{
+				phoneBook.ValidatePhoneNumber(newPhone);
+			}
+			catch (Exception ex)
+			{
+				dialogs.ShowMessageException(ex.InnerException is null ? ex.Message : ex.InnerException.Message, "Ошибка валидации");
+				return;
+			}
+			if (!Phones.Contains(newPhone))
+			{
+				Phones.Add(newPhone);
+				InputNewType = string.Empty;
+				InputNewPhone = string.Empty;
+			}
+			else
+			{
+				dialogs.ShowMessageInfo("Телефон с данным номером уже есть в списке", "Внимание");
+			}
+		}
+
+		private void OnCloseWindow(object obj)
+		{
+			model = null;
+			Close.Invoke();
+		}
+
+		private void OnAddAbonentInPhoneBook(object obj)
+		{
+			model = (AbonentModel)GlobalSelectAbonent.Clone();
+			model.Name = Name;
+			model.Surname = Surname;
+			model.Residence = Residence;
+			model.Phones = Phones.ToArray();
+			model.Groups = SelectedGroups.ToArray();
+			model.DateOfBirth = DateOfBirth;
+			model.ImageBase64 = abonentImageBase64;
+			Close.Invoke();
+		}
+
+		private bool TryUpdateUser(AbonentModel abonent)
+		{
+			try
+			{
+				phoneBook.UpdateAbonent(abonent);
+			}
+			catch (Exception ex)
+			{
+				dialogs.ShowMessageException(ex.InnerException == null ? ex.Message : ex.InnerException.Message, "Ошибка внесения изменений");
+				return false;
+			}
+			return true;
+		}
+
+		private void OnAddImage(object obj)
+		{
+			var result = dialogs.OpenImageFile();
+			if (!string.IsNullOrEmpty(result))
+			{
+				var imageArray = File.ReadAllBytes(result);
+				TryAddImage(imageArray);
+			}
+		}
+
+		private void TryAddImage(byte[] bytes)
+		{
+			abonentImageBase64 = Convert.ToBase64String(bytes);
+			BitmapImage bi = new BitmapImage();
+			bi.BeginInit();
+			bi.StreamSource = new MemoryStream(bytes);
+			bi.EndInit();
+
+			Image nImage = new Image();
+			nImage.Source = bi;
+			AbonentImage = nImage;
+		}
+
+		private void OnDeleteImage(object obj)
+		{
+			abonentImageBase64 = string.Empty;
+			AbonentImage = null;
+		}
 	}
 }
